88d784be66531ec691c60cd1ee7479cc3c1812b2
datadumbo2021 <83563035+datadumbo2021@users.noreply.github.com>
2023-10-18 16:00:57 +0900

    Update stock_predict_app.py

--- stock_predict_app.py
+++ stock_predict_app.py
@@ -1,48 +1,36 @@
-
-
-
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import base64
 import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 st.title('반도체 주식 데이터 Dashboard')
 
-tickers = {'SK hynix':'000660.KS', 'Samsung Electronics':'005930.KS', 'NVIDIA Corporation':'NVDA', 'QUALCOMM':'QCOM'}
+tickers = {'SK hynix': '000660.KS', 'Samsung Electronics': '005930.KS', 'NVIDIA Corporation': 'NVDA', 'QUALCOMM': 'QCOM'}
 
-reversed_ticker = dict(map(reversed, tickers.items()))
+selected_tickers = st.multiselect('Select the ticker symbols you want to visualize:', list(tickers.keys()))
 
-dropdown = st.multiselect('select', tickers.keys())
+start_date = st.date_input('Start Date', pd.to_datetime('2019-01-01'))
+end_date = st.date_input('End Date', pd.to_datetime('today'))
 
-start = st.date_input('Start', value=pd.to_datetime('2019-01-01'))
-end = st.date_input('End', value=pd.to_datetime('today'))
+def get_image_download_link(img, filename, text):
+    buffered = BytesIO()
+    img.savefig(buffered, format="png")
+    img_data = base64.b64encode(buffered.getvalue()).decode()
+    href = f'<a download="{filename}" href="data:image/png;base64,{img_data}">{text}</a>'
+    return href
 
-if len(dropdown) > 0:
-    for i in dropdown:
-        df = yf.download(tickers[i], start, end)['Adj Close']
+if selected_tickers:
+    for ticker in selected_tickers:
+        data = yf.download(tickers[ticker], start=start_date, end=end_date)
+        st.write(f"### {ticker} - Adjusted Close Price")
+        st.line_chart(data['Adj Close'])
 
-        st.title(reversed_ticker[tickers[i]])
-        st.line_chart(df)
-
-        # 이미지로 저장
-        plt.figure(figsize=(10,6))
-        plt.plot(df.index, df.values, label=reversed_ticker[tickers[i]])
-        plt.title(reversed_ticker[tickers[i]])
-        plt.xlabel('Date')
-        plt.ylabel('Price')
-        plt.legend()
-        plt.grid(True)
-
-        # 이미지를 바이트 데이터로 변환
-        from io import BytesIO
-        buffer = BytesIO()
-        plt.savefig(buffer, format="jpg")
-        buffer.seek(0)
-
-        # 바이트 데이터를 base64로 인코딩
-        image_b64 = base64.b64encode(buffer.getvalue()).decode()
-
-        # 다운로드 링크 생성
-        href = f'<a download="{reversed_ticker[tickers[i]]}.jpg" href="data:image/jpg;base64,{image_b64}">Download {reversed_ticker[tickers[i]]} Chart as JPG</a>'
-        st.markdown(href, unsafe_allow_html=True)
+        fig, ax = plt.subplots()
+        ax.plot(data.index, data['Adj Close'])
+        ax.set_title(f'{ticker} Adjusted Close Price')
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price')
+        
+        st.markdown(get_image_download_link(fig, f"{ticker.replace(' ', '_')}_adjusted_close.png", 'Download Image'), unsafe_allow_html=True)
